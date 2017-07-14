3a4b6caaed7ef6c57cfcb13a69debc5d9769f7cb
qqchao <qc9201@sina.com>
2017-07-14 15:48:58 +0800

    linker注册接口返回信息中添加targetdatabase实体json

--- css/src/main/java/com/penghai/css/management/business/impl/LinkerManagerBusinessImpl.java
+++ css/src/main/java/com/penghai/css/management/business/impl/LinkerManagerBusinessImpl.java
@@ -52,6 +52,7 @@ public class LinkerManagerBusinessImpl implements LinkerManagerBusiness{
 		
 		Linker linkerQuery = null;
 		JSONObject resultJson = new JSONObject();
+		String targetDatabaseInfoJson = "";
 		String databaseInfoJson = "";
 		String xmlTargetDatabase = "";
 		String resultMapEncode = "";	
@@ -83,6 +84,7 @@ public class LinkerManagerBusinessImpl implements LinkerManagerBusiness{
 		}else{
 			JSONObject xmlDatabasesJson = goodsFileBusiness.getDatabaseAndTargetDatabaseInfoFromXml(linkerId);
 			
+			targetDatabaseInfoJson = xmlDatabasesJson.getString("targetDatabaseInfoJson");
 			xmlTargetDatabase = xmlDatabasesJson.getString("xmlTargetDatabase");
 			databaseInfoJson = xmlDatabasesJson.getString("databaseInfoJson");
 			//解析xml文件失败
@@ -115,6 +117,7 @@ public class LinkerManagerBusinessImpl implements LinkerManagerBusiness{
 				}
 				
 				//设置返回值
+				resultJson.put("targetDatabaseInfoJson", targetDatabaseInfoJson);
 				resultJson.put("databaseInfoJson", databaseInfoJson);
 				resultJson.put("targetDatabaseXml", xmlTargetDatabase);
 				resultJson.put("code", CM_LINKER_RETURN_CODE.LINKER_RETURN_OK);	
